1318d4099012ccc537d3bbafb4f32974a10a855a
hiepphuc <phuc5411x@gmail.com>
2026-01-21 00:44:33 +0700

    Tạo Schema cho Url (một collection trong db) và export model thông qua mongoose.model

---
+++ models/Url.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { nanoid } = require('nanoid') 
+
+// 1. Định nghĩa Schema (Khuôn mẫu)
+const UrlSchema = new mongoose.Schema({
+    originalUrl: {
+        type: String,
+        required: true,
+        unique: false, // Cho phep trung url goc
+    },
+    shortUrlId: {
+        type: String,
+        required: true,
+        unique: true,
+        default: () => nanoid(8) // Tự động sinh mã 8 ký tự nếu không gửi lên
+    }
+})
+
+// 2. Tạo Model từ Schema và Export
+// 'Url' ở đây sẽ là tên của Collection trong Database (Mongoose sẽ tự đổi thành số nhiều: urls)
+module.exports = mongoose.model('Url', UrlSchema)
