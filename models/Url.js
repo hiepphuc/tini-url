015fd4d549e534bf724e8be046de217aa314de77
hiepphuc <phuc5411x@gmail.com>
2026-01-22 17:23:12 +0700

    Tạo nơi lưu dữ liệu (schema và model) cho user

--- models/Url.js
+++ models/Url.js
@@ -14,8 +14,8 @@ const UrlSchema = new mongoose.Schema({
         unique: true,
         default: () => nanoid(8) // Tự động sinh mã 8 ký tự nếu không gửi lên
     }
-})
+});
 
 // 2. Tạo Model từ Schema và Export
 // 'Url' ở đây sẽ là tên của Collection trong Database (Mongoose sẽ tự đổi thành số nhiều: urls)
-module.exports = mongoose.model('Url', UrlSchema)
+module.exports = mongoose.model('Url', UrlSchema);
