d3abeb8d5c17f3a6ac3b0d5c81d34194716cbb57
hiepphuc <phuc5411x@gmail.com>
2026-01-24 00:08:19 +0700

    Login: Tạo method so sánh password của user gửi request login và password đã hash trong db

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema(
 {
@@ -38,6 +38,10 @@ UserSchema.pre('save', async function (next) {
     } catch (error) {
         return next(error)
     }
-})
+});
+
+UserSchema.methods.matchPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
