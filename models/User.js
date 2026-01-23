c32e56189fac1be65bcd9f85e5e0d5e685db091f
hiepphuc <phuc5411x@gmail.com>
2026-01-23 23:37:50 +0700

    Tạo một save pre-hook để hash password trước khi lưu vào db

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema(
 {
@@ -22,4 +23,21 @@ const UserSchema = new mongoose.Schema(
     timestamps: true
 });
 
-module.exports = mongoose.model('User',UserSchema);
+UserSchema.pre('save', async function (next) {
+    const user = this;
+
+    // Trường hợp user không thay đổi mật khẩu thì kết thúc và đi tiếp luôn (return và next)
+    if (!user.isModified('password')) return next();
+    
+    // Trường hợp user đổi mật khẩu hoặc tạo tài khoản lần đầu (tạo mật khẩu lần đầu)
+    try {
+        const salt = await bcrypt.genSalt(10); // Salt độ phức tạp 10 vòng
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash;
+        next();
+    } catch (error) {
+        return next(error)
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema);
