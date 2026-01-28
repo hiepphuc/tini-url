1f67553d6a0c452e57bacfe650878471b2ef894f
hiepphuc <phuc5411x@gmail.com>
2026-01-28 20:38:41 +0700

    Middle để xác thực-verity token JWT gửi từ client và route GET /me để test middle

--- index.js
+++ index.js
@@ -14,6 +14,37 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Middleware xác thực token gửi từ client
+function verifyToken(req, res, next) {
+    const authHeader = req.header('Authorization');
+
+    // 1. Kiểm tra: Nếu không có authHeader (authorization header) thì chặn luôn
+    if (!authHeader) return res.status(401).json('Unauthorized');
+
+    // 2. Lấy token (bỏ chữ 'Bearer ')
+    const token = authHeader.replace('Bearer ', '');
+    if (!token) return res.status(401).json('Unauthorized');
+
+    // 3. Kiểm tra tính hợp lệ
+    try {
+        // Thử xác thực
+        const verified = jwt.verify(token, 'secret_123');
+        
+        // Nếu ok, gán thông tin user vào request để các route sau dùng được
+        req.user = verified; 
+        
+        // Cho đi tiếp
+        next();
+    } catch (err) {
+        // Nếu lỗi (token đểu, hết hạn...) thì báo lỗi 400 hoặc 401
+        res.status(400).send('Invalid Token');
+    }
+};
+
+// Route để test middle verifyToken
+app.get('/me', verifyToken, (req, res) => {
+    res.json(req.user);
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
